d1466d193324ec20e2533b06e4459d74a0c5db7c
aman89053 <36933525+aman89053@users.noreply.github.com>
2024-05-25 14:46:29 +0530

    Update 10001st_prime_no.php

--- 10001st_prime_no.php
+++ 10001st_prime_no.php
@@ -24,3 +24,40 @@ while ($primeCount < 10001) {
 
 echo  $number;
 ?>
+
+
+
+
+<?php
+///////////////////// Optimized function to check if a number is prime//////////////////////////
+function isPrime($number) {
+    if ($number <= 1) {
+        return false;
+    }
+    if ($number == 2) {
+        return true;
+    }
+    if ($number % 2 == 0) {
+        return false;
+    }
+    for ($i = 3; $i <= sqrt($number); $i += 2) { // Check only odd divisors
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Find the 10001st prime number
+$primeCount = 0;
+$number = 1;
+while ($primeCount < 10001) {
+    $number++;
+    if (isPrime($number)) {
+        $primeCount++;
+    }
+}
+
+echo "The 10001st prime number is: $number";
+?>
+
